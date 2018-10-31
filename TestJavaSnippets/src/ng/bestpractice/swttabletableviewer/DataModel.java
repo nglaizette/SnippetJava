6f01eb8646a9e9024ed6025dd1f62ada0be984b7
NG <7412555+nglaizette@users.noreply.github.com>
2018-10-31 18:04:56 +0100

    Update DataModel.java
    remove stuff

--- TestJavaSnippets/src/ng/bestpractice/swttabletableviewer/DataModel.java
+++ TestJavaSnippets/src/ng/bestpractice/swttabletableviewer/DataModel.java
@@ -1,25 +1,16 @@
-/*
- * Copyright (C) 2004-@year@ by IFP-GROUP.
- * All rights reserved.
- *
- * IFP Headquarters:
- * 1 & 4, avenue de Bois-Preau
- * 92852 Rueil-Malmaison Cedex - France
- */
+
 package ng.bestpractice.swttabletableviewer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * IFP-Group <br>
- * Infrastructure Project <br>
  * <p>
  * DataModel:
  * <p>
  * Created on 15 oct. 2014
  * 
- * @author glaizett
+ * @author ng
  */
 public class DataModel {
     private int samples;
